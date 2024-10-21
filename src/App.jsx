5d826fc1d987046f92dcef3cf0c3ebaeca2ebee8
samikshya <samikshyakharel405@gmail.com>
2024-10-22 09:04:24 +1100

    project initilized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <>
+      <div>Daily grain</div>
+    </>
+  );
+};
+
+export default App;
